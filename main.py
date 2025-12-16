c3006e88b71d3afa483c669a06ef4d1e4169b0e3
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-12-16 15:50:40 +0300

    Классовый метод

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-"""Демомодуль для курса"""
-
-from datetime import date
-
-
-class Book:
-    """Книга"""
-
-    def __init__(self, title: str, year: int) -> None:
-        self.title = title
-        self.year = year
-
-    @staticmethod
-    def year_since(year: int) -> int:
-        return date.today().year - year
+"""Демомодуль для курса
+Классовый метод
+"""
 
 
-book = Book('Властелин колец', 1985)
-print(Book.year_since(book.year))
+class User:
+    """Пользователь"""
+    users = []
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        User.users.append(self)
+
+    @classmethod
+    def from_string(cls, data: str):
+        """Альтернативное создание"""
+        name, age = data.split(',')
+        return cls(name, int(age))
+
+    @classmethod
+    def total_users(cls):
+        """Число пользователей"""
+        return len(cls.users)
+
+
+vasia = User('Вася', 18)
+kate = User('Катя', 20)
+# print(vasia.total_users())
+print(User.total_users())  # Правильнее будет вызвать метод так
+
+maxim = User.from_string('Макс, 40')
+print(maxim.name)
+print(maxim.age)
