6bcdf5cf066ae6194d4ac2cea6e38f4e501d4fe3
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2025-12-16 20:25:41 +0300

    Встроенные (магические) методы

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 """Демомодуль для курса
-Классовый метод
+Встроенные методы
 """
 
 
-class User:
-    """Пользователь"""
-    users = []
+class ShoppingList:
+    """Список покупок"""
 
-    def __init__(self, name: str, age: int) -> None:
-        self.name = name
-        self.age = age
-        User.users.append(self)
+    def __init__(self, items: list[str]) -> None:
+        self.items: list[str] = items
 
-    @classmethod
-    def from_string(cls, data: str):
-        """Альтернативное создание"""
-        name, age = data.split(',')
-        return cls(name, int(age))
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, ShoppingList):
+            return False
+        return self.items == value.items
 
-    @classmethod
-    def total_users(cls):
-        """Число пользователей"""
-        return len(cls.users)
+    def __len__(self):
+        return len(self.items)
+
+    def __getitem__(self, index):
+        return self.items[index]
+
+    def __repr__(self) -> str:
+        # return f'[{', '.join(self.items)}]'
+        return f'ShoppingList[items = {', '.join(self.items)}]'
 
 
-vasia = User('Вася', 18)
-kate = User('Катя', 20)
-# print(vasia.total_users())
-print(User.total_users())  # Правильнее будет вызвать метод так
+list_1 = ShoppingList(['Книга'])
+list_2 = ShoppingList(['Телевизор', 'Компьютер'])
 
-maxim = User.from_string('Макс, 40')
-print(maxim.name)
-print(maxim.age)
+print(len(list_1))
+print(list_1[0])
+print(list_2[1])
+print(list_2)
+
+print(list_1 == list_2)
