665ddd6cbdbcf1dff32983d2fbc889fbe2d12032
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2025-12-17 20:19:06 +0300

    Dataclass

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 """Демомодуль для курса
-Property
+Dataclass
 """
 
-
-class Rectangle:
-    """Прямоугольник"""
-
-    def __init__(self, width: float, height: float) -> None:
-        self.width = width
-        self.height = height
-
-    @property
-    def area(self):
-        """Площадь"""
-        return self.width * self.height
+from dataclasses import dataclass, field
+from datetime import datetime
 
 
-rect = Rectangle(10, 5)
+@dataclass(order=True)
+class Task:
+    """Задача"""
+    title: str
+    sectet_key: str = field(repr=False, compare=False)
+    priority: int = 3
+    done: bool = False
+    crated_at: datetime | None = None
 
-print(rect.area)  # из-за @property, теперь обращаемся к методу как к свойству
+    def __post_init__(self):
+        if self.crated_at is None:
+            self.crated_at = datetime.now()
+
+
+task_1 = Task('Скачать лекцию', 'mysecret')
+task_2 = Task('Скачать лекцию', 'mysecret')
+print(task_1)
+print(task_1 == task_2)
