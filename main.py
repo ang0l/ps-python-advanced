78a7ca8134d148079af465bfa1da1a160cf98b21
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-12-18 15:20:15 +0300

    Декоратор с параметром

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 """Демомодуль для курса
-Декораторы классов
+Декоратор с параметром
 """
 
 
-def add_repr(cls):
-    """Добавлеине repr"""
-
-    def __repr__(self):
-        return f'{cls.__name__}[{self.__dict__}]'
-    cls.__repr__ = __repr__
-    return cls
-
-
-@add_repr
-class User:
-    """Пользователь"""
-
-    def __init__(self, name: str, age: int):
-        self.name = name
-        self.age = age
+def repeat(times: int):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(times):
+                func(*args, **kwargs)
+        return wrapper
+    return decorator
 
 
-user = User('Андрей', 55)
-print(user)
+@repeat(3)
+def hello():
+    print('Привет!')
+
+
+hello()
