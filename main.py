e42c94b951522b523b3caa2a3e57b31a2cda9097
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-12-18 12:20:54 +0300

    Что такое декоратор

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 """Демомодуль для курса
-Dataclass
+Что такое декоратор
 """
 
-from dataclasses import dataclass, field
-from datetime import datetime
+
+def log_decorator(func):
+    """Обертка"""
+    def wrapper():
+        print('Функция началась')
+        func()
+        print('Функция завершилась')
+    return wrapper
 
 
-@dataclass(order=True)
-class Task:
-    """Задача"""
-    title: str
-    sectet_key: str = field(repr=False, compare=False)
-    priority: int = 3
-    done: bool = False
-    crated_at: datetime | None = None
-
-    def __post_init__(self):
-        if self.crated_at is None:
-            self.crated_at = datetime.now()
+@log_decorator
+def say_hello():
+    """Приветствие"""
+    print('Привет!')
 
 
-task_1 = Task('Скачать лекцию', 'mysecret')
-task_2 = Task('Скачать лекцию', 'mysecret')
-print(task_1)
-print(task_1 == task_2)
+say_hello()
