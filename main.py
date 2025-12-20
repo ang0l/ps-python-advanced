fa612d988fd81b2b0d987b0707553ab57ce9f3f9
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2025-12-20 07:35:20 +0300

    Дескриптор

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 """Демомодуль для курса
-Декоратор методов
+Дескриптор
 """
-from functools import wraps
 
 
-def log_call(fn):
-    """Логирует вызов"""
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        print(f'[LOG] {fn.__qualname__} args={args} kwargs={kwargs}')
-        return fn(*args, **kwargs)
-    return wrapper
+class Positive:
+    def __init__(self, name):
+        self.name = name
+
+    def __get__(self, obj, owner):
+        print('Сработал get')
+        return obj.__dict__[self.name]
+
+    def __set__(self, obj, value):
+        print('Сработал set')
+        if value <= 0:
+            raise ValueError(
+                f'Значение "{self.name}" должно быть больше нуля. Ваше значение = {value}')
+        obj.__dict__[self.name] = value
 
 
-class Service:
-    """Сервиснвй"""
-    @log_call
-    def process(self, x: float) -> float:
-        """Удваивание"""
-        return x * 2
+class Product:
+    price = Positive('price')
 
 
-s = Service()
-s.process(10)
-
-
-def a():
-    return 1
-
-
-print(a.__name__)
-print(s.process.__name__)
+p = Product()
+p.price = 100
+print(p.price)
+try:
+    p.price = -1
+except ValueError as e:
+    print(f'[Error]: {e}')
