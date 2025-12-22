db7d593813180f6cefb42c0a05386af2a6b64676
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-12-22 12:59:49 +0300

    Множественное наследование

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
 """Демомодуль для курса
-Наследование
+Множественное наследование
 """
 
 
-class User:
-    """Пользователь платформы"""
-
-    def __init__(self, name: str, email: str):
-        self.name = name
-        self.email = email
-
-    def get_info(self):
-        return f'{self.name}, {self.email}'
+class Light:
+    def turn_on(self):
+        print('Свет включен')
 
 
-class Student(User):
-    """Студетн платформы"""
-
-    def watch_video(self):
-        print('Смотрю')
+class Music:
+    def play(self):
+        print('Музыка включена')
 
 
-class Mentor(User):
-    """Преподаватель платформы"""
+class SmartHome(Light, Music):
+    def start(self):
+        print('Умный дом активен')
+        self.turn_on()
+        self.play()
 
-    def check_homework(self):
-        print('Проверяю')
 
-
-student = Student('Вася', 'a@a.ru')
-print(student.get_info())
-print(student.email)
-print(student.watch_video())
+home = SmartHome()
+home.start()
