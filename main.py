71c20ecbbc3d0274dbf9306e7a79cc118c578377
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-12-23 10:11:54 +0300

    Method Resolution Order

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 """Демомодуль для курса
-Множественное наследование
+Method Resolution Order
 """
 
 
 class Light:
+    """Управление светом"""
+
     def turn_on(self):
+        """Включение света"""
         print('Свет включен')
 
 
 class Music:
-    def play(self):
+    """Управление музыкой"""
+
+    def turn_on(self):
+        """Включение музыки"""
         print('Музыка включена')
 
 
 class SmartHome(Light, Music):
+    """Умный дом"""
+
+    def play(self):
+        """Включение музыки
+        переопределенный метод родительского класса Music
+        """
+        print('Альтернативный play')
+
     def start(self):
+        """Активация умного дома"""
         print('Умный дом активен')
         self.turn_on()
         self.play()
@@ -22,3 +37,5 @@ class SmartHome(Light, Music):
 
 home = SmartHome()
 home.start()
+
+print(SmartHome.mro())
