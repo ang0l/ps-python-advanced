5be54774d2864bb36ed916b197bc809a71990088
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-12-23 11:17:17 +0300

    Упражнение - Курсы

--- main.py
+++ main.py
@@ -1,32 +1,58 @@
 """Демомодуль для курса
-Super"""
+Упражнение - Курсы
+"""
+
+# Делаем класс Курса с ценой, названием, длительностью. Методы:
+# - узнать цену
+# - вывести информацию
+# Делаем курс с AI и тренажерами
+# - можно расчитать рассрочку на срок курса
+# Делаем курс с проектом с параметром названия проекта
+# - можно расчитать рассрочку на срок курса
+# - можно вывести информацию о проекте
 
 
-class Order:
-    """Заказ"""
+class Course:
+    """Курс"""
 
-    def __init__(self, number: int, total: float):
-        self.number = number
-        self.total = total
-        print(f'Создан заказ #{number} на сумму {total}')
+    def __init__(self, price: float, name: str, length: int):
+        self.price = price
+        self.name = name
+        self.length = length
 
-    def process(self):
-        """Оформление заказа"""
-        print('Заказ оформлен')
+    def get_price(self):
+        """Получить цену"""
+        return self.price
 
-
-class EmailOrder(Order):
-    """Заказ по email"""
-
-    def __init__(self, number: int, total: float, email: str):
-        super().__init__(number, total)
-        self.email = email
-
-    def process(self):
-        """Оформление заказа"""
-        super().process()
-        print(f'Письмо отправлено на {self.email}')
+    def get_info(self):
+        """Получить информацию"""
+        return f'Курс {self.name} по цене {self.price} длительностью {self.length}'
 
 
-e = EmailOrder(10, 1, 'a@a.ru')
-e.process()
+class AI(Course):
+    """AI и тренажеры"""
+
+    def calc_credit(self):
+        """Расчет рассрочки"""
+        return self.price / self.length
+
+
+class Project(Course):
+    """Проект"""
+
+    def __init__(self, price: float, name: str, length: int, project_name: str):
+        super().__init__(price, name, length)
+        self.project_name = project_name
+
+    def calc_credit(self):
+        """Расчет рассрочки"""
+        return self.price / self.length
+
+    def get_project_info(self):
+        """Получить информацию"""
+        return f'Проект: {self.project_name}'
+
+
+course = Project(10000, 'Python', 2, 'Кальуклятор')
+print(course.get_info())
+print(course.get_project_info())
