7bc03405023095e83b3754879f187367faafb0b8
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-12-24 10:18:26 +0300

    Упражнение - Расчет скидки

--- main.py
+++ main.py
@@ -1,31 +1,78 @@
 """Демомодуль для курса
-Композиция
+Упражнение - Расчет скидки
 """
-# Notification - уведомление
-# EmailNotification - отправка через e-mail
+# Заказ в интернет магазине
+# Item - name, price, qty и метод subtotal() -> считает цену
+# Политики скидок - NoDiscount - без скидки, PercentageDiscount - процент от заказа
+# Order - list[Item] и политика скидок, метод расчета total total_with_discount и set_policy
+
+from dataclasses import dataclass
 
 
-class Notification:
-    """Уведомление"""
+@dataclass
+class Item:
+    """Единица товара"""
+    name: str
+    price: float
+    qty: int = 1
 
-    def __init__(self, sender):
-        self.sender = sender
-
-    def send(self, message):
-        """Отправка"""
-        self.sender.send(message)
-
-    def get_ack(self):
-        """Еще какой-нибудь метод"""
+    def subtotal(self) -> float:
+        """Расчет суммы"""
+        return self.price * self.qty
 
 
-class EmailSender:
-    """E-mail отправщик"""
+@dataclass
+class PersentageDiscount:
+    """Политика со скидкой"""
+    percent: float
 
-    def send(self, message):
-        """Отправление сообщения"""
-        print(f'Отправлено сообщение {message}')
+    def discount(self, total: float) -> float:
+        return total * (self.percent / 100)
 
 
-notification = Notification(EmailSender())
-notification.send('Сообщение')
+@dataclass
+class PersentageExtraCharge:
+    """Политика с наценкой"""
+    percent: float
+
+    def discount(self, total: float) -> float:
+        return total * (self.percent / 100) * -1
+
+
+class NoDiscount:
+    """Политика без скидки"""
+
+    def discount(self, total: float) -> float:
+        return 0
+
+
+class Order:
+    """Заказ"""
+
+    def __init__(self, items: list[Item], policy):
+        self.items = items
+        self.policy = policy
+
+    def total(self):
+        return sum(i.subtotal() for i in self.items)
+
+    def total_with_discount(self):
+        t = self.total()
+        return t - self.policy.discount(t)
+
+    def set_policy(self, policy):
+        self.policy = policy
+
+
+basket = [Item('Бумага', 100, 5), Item('Ершик', 1000, 1)]
+order = Order(basket, NoDiscount())
+print(order.total())
+print(order.total_with_discount())
+
+order.set_policy(PersentageDiscount(10))
+print(order.total())
+print(order.total_with_discount())
+
+order.set_policy(PersentageExtraCharge(10))
+print(order.total())
+print(order.total_with_discount())
