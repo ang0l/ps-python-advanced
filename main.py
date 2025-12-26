46ca88926db43468d98a6a85209cb432143ebc5b
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-12-26 12:56:04 +0300

    Абстрактный класс

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
 """Демомодуль для курса
-Упражнение - Хранилище
+Абстрактный класс
 """
 # Хранилище
 # Нужно реализовать MevoryStorage и FileStorage с методами load и save
 # Приложение читает строку и передает в use_storage, сохраняет в одном из storage
 # После успешного сохранения читает storage и выводит сохраненные данные
 
+from abc import ABC, abstractmethod
 from typing import Protocol
 
 
-class Storage(Protocol):
+class Storage(ABC):
     """Протокол хранения"""
 
+    @abstractmethod
     def save(self, data: str) -> None: ...
 
+    @abstractmethod
     def load(self) -> str: ...
 
+    def log(self):
+        print('Сохранено')
 
-class MemoryStorage:
+
+class MemoryStorage(Storage):
     """Хранение в памят"""
 
     def save(self, data: str) -> None:
         self.data = data
+        self.log()
 
     def load(self) -> str:
         return getattr(self, 'data', '')
 
 
-class FileStorage:
+class FileStorage(Storage):
     """Хранение в памят"""
 
     def save(self, data: str) -> None:
         with open('data.txt', 'w', encoding='utf-8') as f:
             f.write(data)
+            self.log()
 
     def load(self) -> str:
         with open('data.txt', 'r', encoding='utf-8') as f:
