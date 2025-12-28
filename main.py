68d8c44376d8789e73dad59bdc96f69eecaf9dd9
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2025-12-28 06:28:43 +0300

    Open Closed Principle

--- main.py
+++ main.py
@@ -1,71 +1,57 @@
 """Демомодуль для курса
-Упражнение - Статистика учеников
+Open Closed Principle
 """
 
-# Управление студентами
-# У нас есть студенты с именами и оценками
-# Нужно сделать возможность:
-# - добавить студента в список
-# - получить список студентов
-# - получить среднюю оценку
-# - получить лучшего студента
-# - распечатать отчет - средний балл и лучший студент
+# Принцип открытости и закрытости
+# Классы должны быть открыты для расширения, но закрыты для модификации.
 
+# Неправильный калькулятор
+# class DiscountCalculator:
+#     def calculate(self, user_type: str, amount: float) -> float:
+#         if user_type == 'student':
+#             return amount * 0.9
+#         elif user_type == 'vip':
+#             return amount * 0.8
+#         else:
+#             return amount
+
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
 
-@dataclass
-class Student:
-    name: str
-    grade: int
+class DiscoutPolicy(ABC):
+    @abstractmethod
+    def apply_discount(self, amount: float) -> float:
+        pass
+
+
+class NoDiscount(DiscoutPolicy):
+    def apply_discount(self, amount: float) -> float:
+        return amount
+
+
+class StudentDiscount(DiscoutPolicy):
+    def apply_discount(self, amount: float) -> float:
+        return amount * 0.9
+
+
+class VipDiscount(DiscoutPolicy):
+    def apply_discount(self, amount: float) -> float:
+        return amount * 0.8
+
+
+class GuestDiscount(DiscoutPolicy):
+    def apply_discount(self, amount: float) -> float:
+        return amount * 0.95
 
 
 @dataclass
-class StudentRepository:
-    students: list[Student]
+class DiscountCalculator:
+    policy: DiscoutPolicy
 
-    def add_student(self, student: Student):
-        self.students.append(student)
-
-    def get_list_students(self) -> list[Student]:
-        return self.students
+    def calculate(self, amount: float) -> float:
+        return self.policy.apply_discount(amount)
 
 
-@dataclass
-class StaticticService:
-    repository: StudentRepository
-
-    def get_average_grade(self) -> float:
-        students = self.repository.get_list_students()
-        if not students:
-            return 0
-        total = sum(s.grade for s in students)
-        return total / len(students)
-
-    def get_best_sutdent(self) -> Student:
-        students = self.repository.get_list_students()
-        return max(students, key=lambda s: s.grade)
-
-
-@dataclass
-class ReportPrinter:
-    repository: StudentRepository
-    stat_service: StaticticService
-
-    def print_report(self):
-        print('Отчет по студентам')
-        for s in self.repository.get_list_students():
-            print(f'{s.name}: {s.grade}')
-        print(f'Средний бал: {self.stat_service.get_average_grade()}')
-        best = self.stat_service.get_best_sutdent()
-        print(f'Лучший студент: {best.name}')
-
-
-repo = StudentRepository([Student('Вася', 50)])
-stat_service = StaticticService(repo)
-printer = ReportPrinter(repo, stat_service)
-
-repo.add_student(Student('Аня', 100))
-repo.add_student(Student('Катя', 80))
-
-printer.print_report()
+calc = DiscountCalculator(GuestDiscount())
+print(calc.calculate(1000))
