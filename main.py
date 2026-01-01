d87caae606b98d0c9dd643445a62ab2c07606d56
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2026-01-01 13:43:52 +0300

    Модель объектов

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-"""Демомодуль для курса. Eval Loop"""
+"""Демомодуль для курса. Модель объектов"""
 
-import dis
-
-
-def pr(a):
-    return a + 1
+from typing import Any
 
 
-def sum_new(a, b):
-    c = a + b
-    d = pr(c)
-    return d
+class Demo:
+    def __getattribute__(self, name: str) -> Any:
+        print(f'Доступ к {name}')
+        return super().__getattribute__(name)
+
+    def __getattr__(self, name):
+        print(f'{name} не найден')
+        return None
+
+    def hello(self):
+        print('Привет ', self)
 
 
-x = sum_new(1, 2)
+d = Demo()
+d.x = 42
+print(d.x)
+print(d.y)
 
-dis.dis(sum_new)
+print(d.__dict__)
+print(d.hello)
+print(Demo.hello)
