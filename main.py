6146afaf7fb2635eeba8afb57d2c82317e7ee93f
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2026-01-02 06:24:07 +0300

    Память и сборка мусора

--- main.py
+++ main.py
@@ -1,55 +1,20 @@
-"""Демомодуль для курса. Slots"""
+"""Демомодуль для курса. Память и сборка мусора"""
+
+# import sys
 
 
-from dataclasses import dataclass
-import sys
+# a = [1, 2, 3]
+# b = a
+# del a
+# print(b)
+# print(sys.getrefcount(b))
+
+import gc
 
 
-class A:
-    def __init__(self) -> None:
-        self.x = 1
-        self.y = 1
+a = []
+b = [a]
+a.append(b)
 
-
-print(A.__dict__)
-
-a = A()
-
-
-class B:
-    __slots__ = ('x', 'y')
-
-    def __init__(self) -> None:
-        self.x = 1
-        self.y = 1
-
-
-b = B()
-print(b.x)
-print(b.y)
-# b.z = 1
-
-print(sys.getsizeof(a))
-print(sys.getsizeof(a.__dict__))
-print(sys.getsizeof(b))
-
-
-@dataclass(slots=True)
-class Point:
-    x: int
-    y: int
-
-
-p = Point(1, 1)
-
-
-class D3Point(Point):
-    __slots__ = ('z',)
-
-
-class MyPoint(Point):
-    pass
-
-
-p2 = MyPoint(1, 2)
-print(p2.__dict__)
+print(gc.get_stats())
+gc.collect()
