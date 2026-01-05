750e29abe4e6635db3e11df04def788fb58c972c
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2026-01-05 09:00:48 +0300

    Упражнение - Поиск по id

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
-"""Демомодуль для курса. Generic bound"""
+"""Демомодуль для курса. Упражнение - Поиск по id"""
+
+# Есть User, Product и Order у которых есть поле id.
+# Нужно сделать универвальную функицю поиска по id
+
+
 from dataclasses import dataclass
-from typing import Generic, Protocol, TypeVar
+from typing import Optional, Protocol, TypeVar
 
 
-class Runnable(Protocol):
-    def run(self) -> None: ...
+class Identifiable(Protocol):
+    id: int
 
 
-T = TypeVar('T', bound=Runnable)
+T = TypeVar('T', bound=Identifiable)
 
 
 @dataclass
-class TaskRunner(Generic[T]):
-    tasks: list[T]
+class User:
+    id: int
+    name: str
+    email: str
 
-    def run_all(self) -> None:
-        for task in self.tasks:
-            task.run()
+
+@dataclass
+class Product:
+    id: int
+    title: str
+
+
+@dataclass
+class Order:
+    id: int
+    products: list[Product]
+
+
+def get_by_id(items: list[T], id_: int) -> Optional[T]:
+    for item in items:
+        # проверка на id
+        if item.id == id_:
+            return item
+        return None
+
+
+get_by_id([User(1, 'Андрей', 'a@a.ru')], 1)
