e28e0de46eaf9934236f6d503d0d6930ec434c47
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2026-01-06 07:34:50 +0300

    TypeGuard

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
-"""Демомодуль для курса. Union"""
-
-from typing import Optional, TypeVar, Union
-
-# Union - это объединение двух типов
-# def safe_dif(a: float, b: float) -> Union[float, str]:
+"""Демомодуль для курса. TypeGuard"""
 
 
-def safe_dif(a: float, b: float) -> float | str:
-    if b == 0:
-        return 'Деление на 0'
-    return a / b
+from typing import TypeGuard
 
 
-# def safe_dif2(a: float, b: float) -> float | None:
-def safe_dif2(a: float, b: float) -> Optional[float]:
-    if b == 0:
-        return None
-    return a / b
+def is_int_list(x: list[int] | list[str]) -> TypeGuard[list[int]]:
+    """TypeGuard
+    принимает список из чисел или строк (Union)
+    возвращает тип TypeGuard[list[int]]
+    реально возвращает булево:
+    True если список из чисел, иначе False
+    """
+    return all(isinstance(i, int) for i in x)
 
 
-T = TypeVar('T')
+def is_str_list(x: list[int] | list[str]) -> TypeGuard[list[str]]:
+    """TypeGuard
+    принимает список из чисел или строк (Union)
+    возвращает тип TypeGuard[list[int]]
+    реально возвращает булево:
+    True если список из строк, иначе False
+    """
+    return all(isinstance(i, str) for i in x)
 
 
-# def ensure_list(value: str | list[str]) -> list[str]:
-def ensure_list(value: T | list[T]) -> list[T]:
-    if isinstance(value, list):
-        return value
-    return [value]
+def f(xs: list[int] | list[str]):
+    """Применяем методы для списков из чисел и списков из строк"""
+    if is_int_list(xs):  # проверяем xs на список из чисел
+        xs[0].is_integer()  # можем выбрать методы только для списка из чисел
+    # else:  # казалось бы, можно и так, НО...
+    #     xs[0].capitalize()  # тип снова стал общим и не годен для метдоов строк
+
+    if is_str_list(xs):  # значит проверка xs на список из строк
+        xs[0].capitalize()  # теперь можем выбрать медтды для списка из строк
