83bb84d9e356914c378368c6c20e2ea72d201073
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2026-01-07 07:44:24 +0300

    async await

--- main.py
+++ main.py
@@ -1,48 +1,17 @@
 """Демомодуль для курса
-Упражнение - Модель события"""
+async await"""
 
-# Есть 3 типа событий:
-# ClickEvent - x, y
-# KeyEvent - key
-# ResizeEvent - width, height
-
-# Нужно сделать функцию:
-# def handle_event(ev: ClickEvent | KeyEvent | ResizeEvent) -> str:
-# выводит строку с параметрами события
+import asyncio
 
 
-from dataclasses import dataclass
+async def get_messate():
+    return 'Привет'
 
 
-@dataclass
-class ClickEvent:
-    x: int
-    y: int
+async def main():
+    result = await asyncio.create_task(get_messate())
+    print(result)
 
+# main()
 
-@dataclass
-class KeyEvent:
-    key: str
-
-
-@dataclass
-class ResizeEvent:
-    width: int
-    height: int
-
-
-def handle_event(ev: ClickEvent | KeyEvent | ResizeEvent) -> str:
-    match ev:
-        case ClickEvent(x=x, y=y):
-            return f'Координаты клика {x}*{y}'
-        case KeyEvent(key=key):
-            return f'Нажата клавиша {key}'
-        case ResizeEvent(width=width, height=height):
-            return f'Размер: {width}*{height}'
-        case _:
-            raise ValueError('Неизвестное событие')
-
-
-print(handle_event(ClickEvent(1, 10)))
-print(handle_event(KeyEvent('Enter')))
-print(handle_event(ResizeEvent(1920, 1080)))
+asyncio.run(main())
