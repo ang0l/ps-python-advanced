7f8e23a58c9b18cbbce597d555346e91ce4aab7f
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2026-01-09 08:01:25 +0300

    wait и wait_for

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """Демомодуль для курса
-Упражнение - Параллельные запросы"""
+wait и wait_for"""
 
 import asyncio
 import aiohttp
@@ -7,12 +7,17 @@ import aiohttp
 
 # Полученние в задачах параллельно 10 раз обращение к google.com
 
+async def fetch(session, url):
+    return await asyncio.wait_for(session.get(url), timeout=2)
+
+
 async def main():
     urls = ['https://google.com'] * 10
     async with aiohttp.ClientSession() as session:
-        tasks = [session.get(url) for url in urls]
+        tasks = [asyncio.create_task(fetch(session, url)) for url in urls]
 
-        result = await asyncio.gather(*tasks)
-        print(list(map(lambda x: x.status, result)))
+        done, _ = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+        results = [task.result() for task in done]
+        print(list(map(lambda x: x.status, results)))
 
 asyncio.run(main())
