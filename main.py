f03f329872714b39068d68d193d097b1a3751504
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2026-01-09 20:09:31 +0300

    cancel

--- main.py
+++ main.py
@@ -1,38 +1,24 @@
 """Демомодуль для курса
-Обработка ошибок"""
+cancel"""
 
 import asyncio
 
 
-async def good():
-    print('Начата good')
-    return 1
-
-
-async def bad():
-    print('Начата bad')
-    raise ValueError('Ошибка')
-    # return 1
+async def job():
+    print('Работаю')
+    await asyncio.sleep(5)
+    print('Готово')
 
 
 async def main():
-    # # try:
-    # #     # res = await asyncio.create_task(bad())
-    # #     task = asyncio.create_task(bad())
-    # #     await asyncio.sleep(2)
-    # #     print('Ждем')
-    # #     await task
-    # #     # print(res)
-    # # except ValueError as e:
-    # #     print(e)
-    # task = asyncio.create_task(bad())
-    # task.add_done_callback(lambda t: print('Ошибочка', t.exception()))
+    task = asyncio.create_task(job())
+    await asyncio.sleep(1)
+    task.cancel()
     try:
-        result = await asyncio.gather(bad(), good(), return_exceptions=True)
-        print(result)
-    # except ValueError as e:
-    except ValueError as e:
-        print(e)
+        await task
+    except asyncio.CancelledError:
+        print(task.cancelled())
+        print('Задача отменена')
 
 
 asyncio.run(main())
