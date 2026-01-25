3da8a2db1aa8c53b4725ad82fcdd7701e4c9b68b
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2026-01-25 06:58:27 +0300

    Загрузка папок

--- note_app/widgets/file_tree.py
+++ note_app/widgets/file_tree.py
@@ -1,9 +1,13 @@
 """Модуль виджета Файлового дерева"""
 
+from pathlib import Path
 from textual.containers import VerticalScroll
 from textual.app import ComposeResult
+from textual.events import Mount
 from textual.widgets import Tree
+from textual.widgets._tree import TreeNode
 
+from note_app.domain import Folder
 from note_app.repositories import BaseFolderRerpository
 
 
@@ -21,3 +25,16 @@ class FileTreeWidget(VerticalScroll):
         self._tree = Tree('Заметки')
 
         yield self._tree
+
+    def on_mount(self):
+        root = self.query_one(Tree).root
+        root.data = Folder('data', Path('data'))
+        root.expand()
+
+    def on_tree_node_expanded(self, event: Tree.NodeExpanded) -> None:
+        node: TreeNode[Folder] = event.node
+        node.remove_children()
+        path = node.data.path if node.data else ''
+        folders = self._folder_repo.get_folders_by_path(Path(path))
+        for folder in folders:
+            node.add(folder.name, folder)
